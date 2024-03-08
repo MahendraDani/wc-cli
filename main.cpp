0ca5ecff96de4a6780535c2c952fa99b0eb57e7a
Mahendra Dani <danimahendra0904@gmail.com>
2024-03-08 12:35:16 +0530

    feat: added switch-case for all options

--- main.cpp
+++ main.cpp
@@ -7,12 +7,43 @@ int main(int argc, char **argv) {
     string options[] = {"-c","-l","-w","-m"};
     if(argc == 3){
         string ioption(argv[1]);
-        if(ioption==options[0]){
-        int size = file_size(argv[2]);
-        cout << "Size of the file is: " << size << "\n";
-        }else{
-          cout << "incorrect option";
+        int ioptionc = -1;
+        for(int i=0;i<4;i++){
+          if(argv[1]==options[i]){
+            ioptionc = i;
+            break;
+          }
         }
+        
+        if(ioptionc == -1){
+          cout << "Please enter a valid option\n";
+          cout << "-c: " << "To obtain file size\n";
+          cout << "-l: " << "To obtain number of lines in a file\n";
+          cout << "-w: " << "To obtain number of words in a file\n";
+          cout << "-m: " << "To obtain number of characters in a file\n";
+          return 1;
+        }
+        switch (ioptionc) {
+          case 0:
+            cout << "Size of the file " << argv[2] << " is: " << file_size(argv[2]) << "\n";
+            break;
+          case 1:
+            cout << "Return number of lines";
+            break;
+          case 2:
+            cout << "Return number of words in the file";
+            break;
+          case 3:
+            cout << "Return number of characters in a file";
+            break;
+        }
+
+       // if(ioption==options[0]){
+        //  int size = file_size(argv[2]);
+         // cout << "Size of the file is: " << size << "\n";
+        //}else{
+         // cout << "incorrect option";
+        //}
        
     }else{
       cout << "At least three words required";
